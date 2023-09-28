da212ed10f23a1d71e0974b325f49f9b1a114f03
Eran Chetz <eran@chetz.org>
2023-09-28 09:57:41 +0300

    Initial Commit

---
+++ Open Push Ups Watch App/Open_Push_UpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_Push_UpsApp.swift
+//  Open Push Ups Watch App
+//
+//  Created by Eran on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Open_Push_Ups_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
